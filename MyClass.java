0e8c7ec61ac99b20c692078debac4d5c2c98f032
DamirMug <d2096680@yandex.ru>
2024-06-20 21:18:27 +0500

    создано окно

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+
+public class MyClass {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("мое окно");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+
+    }
+}
+
+
+
+
